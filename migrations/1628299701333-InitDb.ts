8dfd4a8af640878e32a3d2237fd7d916758e21a1
Erick Malta <erickmalta100@gmail.com>
2021-08-07 18:36:40 -0300

    chore(migrations): add initial migration

--- migrations/1628299701333-InitDb.ts
+++ migrations/1628299701333-InitDb.ts
@@ -3,11 +3,57 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class InitDb1628299701333 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      'CREATE TABLE clients (id varchar PRIMARY KEY NOT NULL, name varchar NOT NULL, latitude decimal NOT NULL, longitude decimal NOT NULL, cpf varchar NOT NULL, phone_number varchar NOT NULL, created_at timestamptz NOT NULL, updated_at timestamptz NOT NULL)',
+      'CREATE TABLE clients (id varchar PRIMARY KEY NOT NULL, name varchar NOT NULL, latitude varchar(20) NOT NULL, longitude varchar(20) NOT NULL, cpf varchar NOT NULL, phone_number varchar NOT NULL, created_at timestamptz NOT NULL, updated_at timestamptz NOT NULL)',
+    );
+
+    await queryRunner.query(
+      'CREATE TABLE plans (id varchar NOT NULL PRIMARY KEY, name varchar NOT NULL, price decimal(12,2) NOT NULL, created_at timestamptz NOT NULL, updated_at timestamptz NOT NULL)',
+    );
+
+    await queryRunner.query(
+      'CREATE TABLE users (id varchar NOT NULL PRIMARY KEY, email varchar(100) NOT NULL, name varchar NOT NULL, password varchar(50) NOT NULL, created_at timestamptz NOT NULL, updated_at timestamptz NOT NULL)',
+    );
+
+    await queryRunner.query(
+      'CREATE TABLE orders (id varchar NOT NULL PRIMARY KEY, user_id varchar, client_id varchar, plan_id varchar, status varchar(10) NOT NULL, created_at timestamptz NOT NULL, updated_at timestamptz NOT NULL)',
+    );
+
+    await queryRunner.query(
+      'CREATE TABLE equipments (id varchar NOT NULL PRIMARY KEY, name varchar NOT NULL, manufacturer varchar NOT NULL, price decimal(12,2) NOT NULL, created_at timestamptz NOT NULL, updated_at timestamptz NOT NULL)',
+    );
+
+    await queryRunner.query(
+      'CREATE TABLE storage (id varchar NOT NULL PRIMARY KEY, equipment_id varchar, quantity int NOT NULL, created_at timestamptz NOT NULL, updated_at timestamptz NOT NULL, last_move timestamptz NOT NULL)',
+    );
+
+    await queryRunner.query(
+      'ALTER TABLE orders ADD CONSTRAINT FK_orders_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL ON UPDATE SET NULL',
+    );
+
+    await queryRunner.query(
+      'ALTER TABLE orders ADD CONSTRAINT FK_orders_plan FOREIGN KEY (plan_id) REFERENCES plans (id) ON DELETE SET NULL ON UPDATE SET NULL',
+    );
+
+    await queryRunner.query(
+      'ALTER TABLE orders ADD CONSTRAINT FK_orders_client FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE SET NULL ON UPDATE SET NULL',
+    );
+
+    await queryRunner.query(
+      'ALTER TABLE storage ADD CONSTRAINT FK_storage_equipment FOREIGN KEY (equipment_id) REFERENCES equipments (id) ON DELETE SET NULL ON UPDATE SET NULL',
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE clients`);
+    await queryRunner.query('ALTER TABLE orders DROP CONSTRAINT FK_orders_user');
+    await queryRunner.query('ALTER TABLE orders DROP CONSTRAINT FK_orders_plan');
+    await queryRunner.query('ALTER TABLE orders DROP CONSTRAINT FK_orders_client');
+    await queryRunner.query('ALTER TABLE storage DROP CONSTRAINT FK_storage_equipment');
+
+    await queryRunner.query('DROP TABLE storage');
+    await queryRunner.query('DROP TABLE equipments');
+    await queryRunner.query('DROP TABLE orders');
+    await queryRunner.query('DROP TABLE users');
+    await queryRunner.query('DROP TABLE plans');
+    await queryRunner.query('DROP TABLE clients');
   }
 }
