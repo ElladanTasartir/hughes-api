6b093b0b06ced0ca156379af30f4f9353fef46d4
Erick Malta <erickmalta100@gmail.com>
2021-09-19 19:56:38 -0300

    chore(migrations): add email to clients

---
+++ migrations/1632091637125-AddEmailToClient.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddEmailToClient1632091637125 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE clients ADD COLUMN email varchar');
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE clients DROP COLUMN emaiç');
+  }
+}
